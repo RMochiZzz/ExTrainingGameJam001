db9461715a142c35eec9835b775285127886b3f8
Shunya Miyamoto <krnmeradrs@gmail.com>
2023-06-06 10:24:32 +0900

    Create: Create PlayerBullet

---
+++ GameJam001/Assets/Scripts/Core/Weapon/PlayerBulllet.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Combat.PlayerAttackMotion
+{
+    public class PlayerBulllet : MonoBehaviour
+    {
+        [SerializeField] private float Bulletspeed;
+        [SerializeField] private float maxDistance;
+
+        private Rigidbody2D rb;
+        private Vector3 defaultPos;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            defaultPos = transform.position;
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            float d = Vector3.Distance(transform.position, defaultPos);
+
+            //Å‘åˆÚ“®‹——£‚ð’´‚¦‚Ä‚¢‚é
+            if (d > maxDistance)
+            {
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                rb.MovePosition(transform.position += transform.up * Time.deltaTime * Bulletspeed);
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+}
